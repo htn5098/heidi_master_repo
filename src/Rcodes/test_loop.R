d97600d07205714651c9c9e56ef687784ec4cf29
htn5098 <htn5098@psu.edu>
2020-03-25 18:46:18 -0400

    Changing the test_loop.R code

--- src/Rcodes/test_loop.R
+++ src/Rcodes/test_loop.R
@@ -4,6 +4,7 @@
 library(foreach)
 library(doParallel)
 library(parallel)
+library(ncdf4)
 
 # Registering cores for parallel processing
 no_cores <- detectCores() #24 cores per node - enough for parallel processing
@@ -12,14 +13,19 @@ cl <- makeCluster(no_cores)
 registerDoParallel(cl)
 
 # Reading the .nc file 
-print("Test loop")
-clusterEvalQ(cl,.libPaths("/storage/home/htn5098/local_lib/R35")) # Really have to import library paths into the workers
-foreach(i = 1:10) %dopar% { #ncol(var.matrix.sa)
-  print(i)
-#  outfile=data.frame(var.matrix.sa[,i])
-#  grid=colnames(var.matrix.sa)[i]
-#  outfilename=paste0('./data/interim/UW_',period,'_',var,'_',grid,'.csv')
-#  write.csv(outfile,outfilename,row.names=F,col.names=F)
-}
+#print("Test loop")
+#invisible(clusterEvalQ(cl,.libPaths("/storage/home/htn5098/local_lib/R35"))) # Really have to import library paths into the workers
+#foreach(i = 1:10) %dopar% { #ncol(var.matrix.sa)
+#  print(i)
+##  outfile=data.frame(var.matrix.sa[,i])
+##  grid=colnames(var.matrix.sa)[i]
+##  outfilename=paste0('./data/interim/UW_',period,'_',var,'_',grid,'.csv')
+##  write.csv(outfile,outfilename,row.names=F,col.names=F)
+#}
+ret_files <- list.files(path = '/storage/home/htn5098/scratch/DataAnalysis/data/raw/gridMET',pattern = 'pr_*', full.names = T)
+nc <- nc_open(ret_files)
+print(ret_files[1])
+print(nc)
+names(nc$var)
 print("Completed")
 stopCluster(cl)
