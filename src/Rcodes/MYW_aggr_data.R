6afaa4aced9217e7ccc778c9623ee20dfbbb8956
htn5098 <htn5098@psu.edu>
2020-03-29 15:54:52 -0400

    Adding options to aggregate to grid or county level

--- src/Rcodes/MYW_aggr_data.R
+++ src/Rcodes/MYW_aggr_data.R
@@ -1,5 +1,8 @@
 inputs = commandArgs(trailingOnly=T)
 namevar=as.character(inputs[1])
+level=as.character(inputs[2])
+
+print(level)
 
 .libPaths("/storage/home/htn5098/local_lib/R35") # needed for calling packages
 .libPaths()
@@ -22,9 +25,10 @@ registerDoParallel(cl)
 #years <- 1979:2018
 #time <- seq.Date(as.Date('1979-01-01'), as.Date('2018-12-31'), by ='days')
 csvfiles <- list.files(path = interim,pattern = paste0('^gridMET.*',namevar), full.names = T) # changing path to gridMET files
+#print(csvfiles)
 gridpoint <- read.table("./data/external/ret_indx_clean.txt",sep = ',', header = T)
-indx <- sort(unique(gridpoint$Grid))
-county <- sort(unique(gridpoint$COUNTYNS))
+column.names <- list(grid = sort(unique(gridpoint$Grid)),
+                    county = sort(unique(gridpoint$COUNTYNS)))
 
 # Aggregating gridded data to county level
 print(paste("Start of aggregating grid to county level"))
@@ -32,19 +36,27 @@ invisible(clusterEvalQ(cl,.libPaths("/storage/home/htn5098/local_lib/R35"))) # A
 aggr.data <- foreach(i = csvfiles,.combine=rbind) %dopar% {
   library(foreach)
   grid.data <- read.csv(i,header=T, check.names=F)
-  county.data <- foreach(i = seq_along(county), .combine = cbind) %do% {
-    pointid <- as.character(gridpoint$Grid[gridpoint$COUNTYNS == county[i]])
-    wt <- gridpoint$Area[gridpoint$COUNTYNS == county[i]]
-    var <- grid.data[,pointid]
-    if(is.null(dim(var))) {
-      aggr = 0
-    } else { 
-      aggr <- apply(var,1,weighted.mean,w = wt,na.rm = T)
+  print(head(grid.data[,1:5]))
+  if (level=="county") {
+    county.data <- foreach(i = seq_along(column.names$county), .combine = cbind) %do% {
+      pointid <- as.character(gridpoint$Grid[gridpoint$COUNTYNS == county[i]])
+      wt <- gridpoint$Area[gridpoint$COUNTYNS == county[i]]
+      var <- grid.data[,pointid]
+      if(is.null(dim(var))) {
+        aggr = 0
+      } else { 
+        aggr <- apply(var,1,weighted.mean,w = wt,na.rm = T)
+      }
     }
-  }
+    return(county.data)
+  } else {
+    return(grid.data)
+  }  
 }
+
 aggr.data.df <- data.frame(aggr.data)
-colnames(aggr.data.df) <- as.character(county)
-write.csv(aggr.data,paste0(outputpath,"GridMET_",namevar,"_county_daily.csv"),row.names=F)
-print("End of aggregation")
+colnames(aggr.data.df) <- as.character(column.names[[level]]) #indx for pdsi, county for precipitation and pet
+head(aggr.data.df[,1:10])
+#write.csv(aggr.data,paste0(outputpath,"GridMET_",namevar,"_county_daily.csv"),row.names=F)
+
 stopCluster(cl)
