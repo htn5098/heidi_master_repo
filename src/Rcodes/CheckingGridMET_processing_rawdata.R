88e797e95d299e30aa45f38c73900953c3140e6d
htn5098 <htn5098@psu.edu>
2020-04-01 20:58:20 -0400

    R code for processing raw gridMET data

---
+++ src/Rcodes/CheckingGridMET_processing_rawdata.R
@@ -0,0 +1,38 @@
+.libPaths("/storage/home/htn5098/local_lib/R35") # needed for calling packages
+.libPaths()
+setwd('/storage/work/h/htn5098/DataAnalysis/') # changing working directory
+inputpath='/storage/home/htn5098/scratch/DataAnalysis/data/raw/gridMET_check/'
+interim= '/storage/home/htn5098/scratch/DataAnalysis/data/interim/' # 
+
+library(foreach)
+library(doParallel)
+library(parallel)
+library(ncdf4)
+
+no_cores <- detectCores()
+cl <- makeCluster(no_cores-1)
+registerDoParallel(cl)
+
+ncfiles <- list.files(path = inputpath, full.names = T) # changing path to gridMET files
+gridpoint <- read.table("./data/external/ret_indx_clean.txt",sep = ',', header = T)
+indx <- sort(unique(gridpoint$Grid))
+
+# Extracting data from .nc files
+print("Starting extracting data from individual .nc files")
+invisible(clusterEvalQ(cl,.libPaths("/storage/home/htn5098/local_lib/R35"))) # Always import the library path to the workers
+foreach(i = ncfiles,.verbose=F,.errorhandling='remove') %dopar% { 
+  library(ncdf4)
+  nc <- nc_open(i)
+  varatt <- ncatt_get(nc,varid=names(nc$var))
+  dt <- ncvar_get(nc,varid = names(nc$var),start = c(795,210,1), count = c(391,266,-1))
+  dim <- dim(dt) 
+  dt.m <- aperm(dt, c(3,2,1)) 
+  dim(dt.m) <- c(dim[3],dim[2]*dim[1])
+  dt.sd <- dt.m[,indx]
+  dt.df <- data.frame(dt.sd)
+  colnames(dt.df) <- as.character(indx)  
+  write.csv(dt.df,paste0(interim,'gridMET_',varatt$standard_name,'.csv'),row.names=F)
+}
+print("End of extracting")
+
+stopCluster(cl)
