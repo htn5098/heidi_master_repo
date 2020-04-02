f785341e4f63f15038298333bd7b53509e2bb977
htn5098 <htn5098@psu.edu>
2020-04-01 20:56:35 -0400

    R code for printing metadata file

--- src/Rcodes/CheckingGridMET_metadata_climate.R
+++ src/Rcodes/CheckingGridMET_metadata_climate.R
@@ -1,33 +1,26 @@
-args = commandArgs(trailingOnly=TRUE)
-link=args[1]
-folder=args[2]
-
-path= paste0("/storage/home/htn5098/work/DataAnalysis/data/raw/UW_clim/",folder)
-
 .libPaths("/storage/home/htn5098/local_lib/R35")
 .libPaths()
 
-library(googledrive)
 library(ncdf4)
 
 setwd("/storage/home/htn5098/scratch/DataAnalysis/data/raw/gridMET_check")
 
-files=list.files(full.names=T)
-for (i in files) {
-  nc = nc.open(i)
-  var = names(nc$var)
-  lat = ncatt_get(nc,vaird='lat')
-  lon = ncatt_get(nc,varid='lon')
-  varatt = ncatt_get(nc,varid=var)
-  #sink("/storage/work/h/htn5098/DataAnalysis/data/log_files/GridMET_metadata.txt")
-  print(i)
-  print(var)
-  print(varatt)
-  print(lat)
-  print(lon)
-  #sink()
+files=list.files(full.names=T)#
+sink("/storage/work/h/htn5098/DataAnalysis/data/log_files/GridMET_metadata.txt",append=T)
+for (i in files) { 
+  nc = nc_open(i)
+  varatt = ncatt_get(nc,varid=names(nc$var))
+  lat = ncvar_get(nc,varid='lat')
+  lon = ncvar_get(nc,varid='lon') 
+  cat(paste('Filename:', i,'\n'))
+  cat(paste('\tVariable id:',names(nc$var),'\n'))
+  cat(paste('\tVariable std name:',varatt$standard_name,'\n'))
+  cat(paste('\tVariable unit:',varatt$units,'\n'))
+  cat(paste('\tLattitude range is:',round(lat[1],3),'to',round(lat[length(lat)],3),' Unit:',nc$dim[[2]]$units,'\n'))
+  cat(paste('\tLongtitude range is:',round(lon[1],3),'to',round(lon[length(lon)],3),' Unit:',nc$dim[[1]]$units,'\n'))
+  cat('\n') 
 }
-
+sink()
 
 
 
